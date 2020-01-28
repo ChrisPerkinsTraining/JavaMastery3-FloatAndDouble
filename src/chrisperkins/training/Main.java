cec0dc233c7d201dda1ac4d1cc033b5631da3df0
Chris Perkins <chrisperkins.cap@gmail.com>
2020-01-28 13:00:27 +0100

    Maximum and Minimum values for float and double. Defacto types (int and double) and using 8.89d and 8.89f notation sometimes required but, generally a good practice.

--- src/chrisperkins/training/Main.java
+++ src/chrisperkins/training/Main.java
@@ -3,6 +3,20 @@ package chrisperkins.training;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value: " + myMinFloatValue);
+        System.out.println("Float maximum value: " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value: " + myMinDoubleValue);
+        System.out.println("Double maximum value: " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5d;
+        
     }
 }
